1a50c4fcb5624add7085cc3efc4d747d32c4c068
MattMarzz <im.matteomarzella@gmail.com>
2024-01-29 15:36:57 +0100

    test db role change

--- src/main/java/it/uniroma2/dicii/bd/controller/SegreteriaController.java
+++ src/main/java/it/uniroma2/dicii/bd/controller/SegreteriaController.java
@@ -1,8 +1,20 @@
 package main.java.it.uniroma2.dicii.bd.controller;
 
+import main.java.it.uniroma2.dicii.bd.enums.Role;
+import main.java.it.uniroma2.dicii.bd.utils.DbConnection;
+
+import java.sql.SQLException;
+
 public class SegreteriaController implements Controller{
     @Override
     public void start() {
+        //first of all change db connection role
+        try {
+            DbConnection.changeRole(Role.SEGRETERIA);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
+        int i = 0;
     }
 }
